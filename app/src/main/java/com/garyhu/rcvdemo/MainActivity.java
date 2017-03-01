dc084a5adac5c64cac08fd9125660a14c8e8a620
garyhu <hww9081020@163.com>
2017-03-01 17:51:04 +0800

    first commit

---
+++ app/src/main/java/com/garyhu/rcvdemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.garyhu.rcvdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            }
+        });
+        PinnedHeaderListView listView = (PinnedHeaderListView) findViewById(R.id.pinnedListView);
+        LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout header1 = (LinearLayout) inflator.inflate(R.layout.list_item, null);
+        ((TextView) header1.findViewById(R.id.textItem)).setText("HEADER 1");
+        LinearLayout header2 = (LinearLayout) inflator.inflate(R.layout.list_item, null);
+        ((TextView) header2.findViewById(R.id.textItem)).setText("HEADER 2");
+        LinearLayout footer = (LinearLayout) inflator.inflate(R.layout.list_item, null);
+        ((TextView) footer.findViewById(R.id.textItem)).setText("FOOTER");
+        listView.addHeaderView(header1);
+        listView.addHeaderView(header2);
+        listView.addFooterView(footer);
+        TestSectionedAdapter sectionedAdapter = new TestSectionedAdapter();
+        listView.setAdapter(sectionedAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
